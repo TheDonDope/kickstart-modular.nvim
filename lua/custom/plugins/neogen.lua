3a2e588e081c944431dcf5a99b20ebbd6613b304
TheDonDope <thedondope@hey.com>
2024-03-18 23:27:42 +0100

    fix: clean up neogen plugin

--- lua/custom/plugins/neogen.lua
+++ lua/custom/plugins/neogen.lua
@@ -1,9 +1,5 @@
 return {
     "danymat/neogen",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "L3MON4D3/LuaSnip",
-    },
     config = function()
         local neogen = require("neogen")
 
