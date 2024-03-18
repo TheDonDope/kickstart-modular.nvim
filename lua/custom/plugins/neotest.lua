1f7cef941ff4f170a9edf303ea2e7db61e0ebd49
TheDonDope <thedondope@hey.com>
2024-03-18 23:19:46 +0100

    feat: add neotest plugin

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            local neotest = require("neotest")
+            neotest.setup({
+                adapters = {
+                    require("neotest-plenary").setup({
+                        -- this is my standard location for minimal vim rc
+                        -- in all my projects
+                        min_init = "./scripts/tests/minimal.vim",
+                    }),
+                }
+            })
+
+            vim.keymap.set("n", "<leader>tc", function()
+                neotest.run.run()
+            end)
+
+            vim.keymap.set("n", "<leader>tf", function()
+                neotest.run.run(vim.fn.expand("%"))
+            end)
+        end,
+    },
+}
