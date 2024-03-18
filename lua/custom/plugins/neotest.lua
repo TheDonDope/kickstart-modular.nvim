0c7fc4cc2577bef06319968aa7fd3960aa0474de
TheDonDope <thedondope@hey.com>
2024-03-18 23:33:08 +0100

    fix: add missing required plugin for neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -6,6 +6,7 @@ return {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/neotest-plenary",
         },
         config = function()
             local neotest = require("neotest")
