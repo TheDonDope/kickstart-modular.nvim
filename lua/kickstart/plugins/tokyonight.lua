c5e464e7053ae1073819325177903224e4e46619
TheDonDope <thedondope@hey.com>
2024-03-23 21:48:26 +0100

    feat: add tokyonight color scheme options

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -15,13 +15,22 @@ return {
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
---    opts = {
---      transparent = true,
---      styles = {
---        sidebars = 'transparent',
---        floats = 'transparent',
---      },
---    },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "storm",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      transparent = true,               -- Enable this to disable setting the background color
+      terminal_colors = true,           -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark",             -- style for sidebars, see below
+        floats = "dark",               -- style for floating windows
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
